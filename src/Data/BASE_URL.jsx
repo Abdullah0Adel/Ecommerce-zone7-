80c1db7ba643392b6ad6bd5caa41f1bb91b0ba48
Abdullah Adel <abdullahadel365@gmail.com>
2025-06-25 15:39:48 +0200

    changing API url from local to rawilway API  link

--- src/Data/BASE_URL.jsx
+++ src/Data/BASE_URL.jsx
@@ -3,6 +3,10 @@ const BASE_URL = `${process.env.REACT_APP_API_URL}/api/`;
 export default BASE_URL;
 
 
+
+console.log("API_URL:", process.env.REACT_APP_API_URL);
+
+
 // const BASE_URL = 'http://localhost:1337/api/';
 
 // export default BASE_URL
