5403ca981a87290e601ed0ab3e79ce3e42689660
Abdullah0Adel <abdullahadel365@gmail.com>
2026-02-27 16:02:27 +0200

    just commit

--- src/Pages/Rigister/Register.jsx
+++ src/Pages/Rigister/Register.jsx
@@ -281,6 +281,7 @@ export default function Register() {
                   </Form>
                 )}
               </Formik>
+              {/* just comment */}
             </div>
           </div>
         </div>
